8f754e3b474b6ad9fb16818b78be540b0ab89c40
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-04-19 02:52:45 +0530

    third

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,27 +3,70 @@ import React from 'react'
 const About = () => {
   return (
    <>
-   <div className='bg-indigo-50 w-[100%] h-[70vh] flex px-32 py-36'>
-  <div className='w-[60%]'>
-    <section className=' w-[70%]'>
-    <p className='text-5xl font-sans font-semibold mb-6'>About Ecoyaan</p>
-    <p className='text-2xl pb-4 tracking-wide text-gray-800'>At Ecoyaan, we are more than just a platform. Our goal is to build a community of eco-conscious people who share a common vision and passion for a more sustainable world.</p>
-    <p className='text-2xl pb-4 tracking-wide text-gray-800'>We recognize that sustainability is a journey (as the Sanskrit word “yaan” suggests). To keep you motivated on this journey, on our platform and our social media pages, you can find:</p>
-    <ul className='text-xl text-gray-800 list-disc px-6'>
+   <div className='bg-indigo-50 w-[100%] md:h-[70vh] flex px-4 md:px-28 py-24 md:py-36 flex-col md:flex-row'>
+  <div className='md:w-[60%]'>
+    <section className=' w-[100%] md:w-[70%]'>
+    <p className='text-2xl md:text-5xl font-sans font-bold mb-6'>About Ecoyaan</p>
+    <p className='text-lg md:text-2xl pb-4 tracking-wide text-gray-800'>At Ecoyaan, we are more than just a platform. Our goal is to build a community of eco-conscious people who share a common vision and passion for a more sustainable world.</p>
+    <p className='text-lg md:text-2xl pb-4 tracking-wide text-gray-800'>We recognize that sustainability is a journey (as the Sanskrit word “yaan” suggests). To keep you motivated on this journey, on our platform and our social media pages, you can find:</p>
+    <ul className='md:text-xl text-gray-800 list-disc px-6'>
       <li>Tips and tricks to adopt a more eco-friendly and low-waste lifestyle</li>
       <li>Videos, posts, and quizzes on climate change and sustainability</li>
       <li>Events and pledges that invite you to adopt a more sustainable lifestyle</li>
     </ul>
     </section>
   </div>
-  <div className='w-[40%] hover:scale-110 transition ease-in-out delay-100'>
-    <img src='https://ecoyaan.com/images/about-us-sustainable-lifestyle-partner.png'/>
+  <div className='w-[80%] mt-12 mx-auto md:w-[40%] hover:scale-110 transition ease-in-out delay-100 '>
+    <img src='https://ecoyaan.com/images/about-us-sustainable-lifestyle-partner.png' 
+    className='rounded-xl'
+    />
   </div>
    </div>
-   <div className='bg-yellow-50 w-[100%] h-[70vh] flex px-32 py-16'>
-    <p className='text-5xl font-sans font-semibold'>Our Values</p>
-    
+   {/* ---------------------------------------------------------------------------------------- */}
+   <div className='bg-yellow-50 w-[100%] h-[70vh] flex px-28 py-12 flex-col'>
+    <p className='text-2xl md:text-5xl font-sans font-semibold md:mb-8'>Our Values</p> 
+     <div className='flex flex-col'>
+      <div className='flex w-[100%] '>
+      <div className='flex'>
+        <img src='https://ecoyaan.com/images/Trust.png' className='w-[50%] h-[50%]'/>
+        <section>
+          <p className='md:text-2xl font-semibold tracking-wider mb-1'>Trust</p>
+          <p className='text-xl tracking-wide'>We value the trust of our community. We strive to be transparent and honest in everything we do, from the content we share today to sourcing our products in the future.</p>
+        </section>
+      </div>
+      <div className='flex'>
+        <img src='https://ecoyaan.com/images/Authenticity.png' className='w-[50%] h-[50%]'/>
+        <section>
+          <p className='md:text-2xl font-semibold tracking-wider mb-1'>Authenticity</p>
+          <p className='text-xl tracking-wide'>We are genuine in our mission to promote sustainability and environmental awareness. An authentic community of eco-conscious people can inspire each other on the journey towards sustainability.</p>
+        </section>
+      </div>
+     </div>
+
+     <div className='flex '>
+      <div className='flex'>
+        <img src='https://ecoyaan.com/images/Impact.png' className='w-[50%] h-[50%]'/>
+      <section>
+      <p className='md:text-2xl font-semibold tracking-wider mb-1'>Impact</p>
+        <p className='text-xl tracking-wide'>We measure our success by our impact on the planet and our community. We aim to empower our sellers and customers to make responsible choices that benefit their well-being and the well-being of others.</p>
+      </section>
+      </div>
+      <div className='flex'>
+        <img src='https://ecoyaan.com/images/FunAndEngaging.png' className='w-[50%] h-[50%]'/>
+       <section>
+       <p className='md:text-2xl font-semibold tracking-wider mb-1'>Fun & Engaging</p>
+        <p className='text-xl tracking-wide'>Moving towards a sustainable lifestyle and the constant news around climate change can be stressful. We aim to inject fun and creativity into everything we do, from designing our products to providing services.</p>
+       </section>
+      </div>
+     </div>
+
+     </div>
    </div>
+{/* __________________________________________________________________________________________________ */}
+<div className='bg-indigo-50 w-[100%]  h-[70vh] flex px-28 py-12 flex-col'>
+     <p className='text-2xl md:text-5xl font-sans font-semibold md:mb-8'>Our Story</p> 
+     
+     </div>
 
    </>
   )
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -50,43 +50,43 @@ const Home = () => {
       </Carousel> 
      </div>
 
-      <div className='h-[80vh] bg-green-100 flex -my-14 home 2'>
-     <div className=' w-full md:w-[60%] bg-white h-[70%] my-auto rounded-r-[100px] flex flex-col justify-center  '> 
-      <section className='w-full md:w-[70%] mx-auto pr-28 break-all contain'>
-      <p className='text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Join our community in creating a more sustainable future for everyone</p>
-     <p className='text-xl mb-4 flex-wrap '>At Ecoyaan, we are more than just a platform. Our vision is to build an eco-conscious community for people and products.</p>
-     <p className='text-xl flex-wrap '>Consider us your partner for all things sustainable. To keep you motivated on the journey, we will provide you with engaging information about climate change, the latest updates on climate policies and lifestyle tips that you can adopt to reduce your impact on the environment.</p>
+      <div className='h-[70vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2 '>
+     <div className=' box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-r-[100px] flex flex-col justify-center '> 
+      <section className='w-[70%] mx-auto md:pr-28 break-all'>
+      <p className='boxHeading text-2xl md:text-4xl font-sans font-semibold flex flex-wrap md:tracking-wide mb-6'>Join our community in creating a more sustainable future for everyone</p>
+     <p className=' boxPara text-xl md:mb-4 md:flex-wrap flex-shrink-3'>At Ecoyaan, we are more than just a platform. Our vision is to build an eco-conscious community for people and products.</p>
+     <p className='text-xl flex-wrap pb-4'>Consider us your partner for all things sustainable. To keep you motivated on the journey, we will provide you with engaging information about climate change, the latest updates on climate policies and lifestyle tips that you can adopt to reduce your impact on the environment.</p>
       </section>
      </div>
 
-     <img src='https://ecoyaan.com/images/Impact.png' alt='image2' className=' joinImage h-[46vh] w-[40%] relative top-[18%] md:w-auto'/>     
+     <img src='https://ecoyaan.com/images/Impact.png' alt='image2' className=' joinImage md:h-[46vh] md:w-[40%] relative md:top-[18%] top-[13%]'/>     
       </div>
       
       
-      <div className='h-[80vh] bg-green-100 flex home3'>
-      <img src={image2} alt='image2' className=' joinImage h-[50vh] w-[40%] relative top-[18%] rounded-full p-10'/>  
-     <div className='w-[60%] bg-white h-[70%] my-auto rounded-l-[100px] flex flex-col justify-center  '> 
-      <section className='w-[70%] mx-auto pr-28 break-all'>
-      <p className='text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Carefully curated Eco-friendly products</p>
+      <div className='h-[100vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2'>
+      <img src={image2} alt='image2' className=' joinImage h-[50vh] md:w-[40%] relative md:top-[18%] top-[70%] rounded-full p-10'/>  
+     <div className='box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-l-[100px] flex flex-col justify-center relative md:-top-0 -top-64'> 
+      <section className='w-[70%] mx-auto md:pr-28 break-all'>
+      <p className='text-2xl md:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Carefully curated Eco-friendly products</p>
      <p className='text-xl mb-4 flex-wrap '>We believe that every purchase you make can have a positive impact on the planet and the future.</p>
      <p className='text-xl flex-wrap '>Soon, we will be connecting you with eco-friendly and sustainable products that are carefully selected based on their environmental and social benefits.</p>
       </section>
      </div>   
       </div>
 
-      <div className='h-[80vh] bg-green-100 flex -my-14 home4'>
-     <div className='w-[60%] bg-white h-[70%] my-auto rounded-r-[100px] flex flex-col justify-center  '> 
-      <section className='w-[70%] mx-auto pr-28 break-all'>
-      <p className='text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>For businesses that care deeply about sustainability</p>
+      <div className='h-[120vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2 '>
+     <div className='box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-r-[100px] flex flex-col justify-center '> 
+      <section className='w-[70%] mx-auto md:pr-28 break-all '>
+      <p className='text-2xl md:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>For businesses that care deeply about sustainability</p>
      <p className='text-xl mb-4 flex flex-wrap '>Do you run a business that is committed to sustainability in every aspect of your work?</p>
      <p className='text-xl mb-4 flex flex-wrap'>Do you want to showcase your products to an incredible audience of eco-conscious consumers who care?</p>
      <p className='text-xl flex flex-wrap mb-5'>If yes, then you are the perfect fit for our platform. Our community appreciates and supports businesses that are transparent, ethical, and innovative in their approach to sustainability. Contact us today:</p>
-     <button className='bg-green-600 w-32 h-12 text-white rounded-3xl text-lg font-medium hover:bg-green-700 cursor-pointer'>Contact us</button>
+     <button className='bg-green-600 md:w-32 w-24 md:h-12 text-white rounded-3xl md:text-lg font-medium hover:bg-green-700 cursor-pointer p-2'>Contact us</button>
       </section>
      </div>
 
-      <div className='w-[40%]'>
-      <img src='https://ecoyaan.com/images/OurStory4.png' alt='image2' className=' mx-auto joinImage h-[46vh] w-[80%] relative top-[18%]'/>     
+      <div className='md:w-[40%]'>
+      <img src='https://ecoyaan.com/images/OurStory4.png' alt='image2' className=' mx-auto joinImage md:h-[46vh] md:w-[80%] relative md:top-[18%] -top-16'/>     
       </div>
       </div>
     </>
