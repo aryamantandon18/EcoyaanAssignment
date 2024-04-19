c103c6f1fed4232aa1562dbde128862e3eea3abd
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-04-20 01:55:39 +0530

    finalCommit

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -8,7 +8,7 @@ const TeamCard = ({image,name,role,para}) => {
         <p className='md:text-3xl font-semibold my-2'>{name}</p>
         <p className='md:text-2xl font-normal text-red-500 py-2 mb-4'>{role}</p>
        </div>
-        <p className='text-2xl flex text-justify tracking-wide w-[100%] font-normal text-[#8d8f8f]'>{para}</p>
+        <p className='text-lg md:text-2xl flex text-justify tracking-wide w-[100%] font-normal text-[#8d8f8f]'>{para}</p>
     </div>
   )
 }
