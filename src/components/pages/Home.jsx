03f74b997e45432b7bcf0a9da91026811db4ff24
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-04-20 20:22:23 +0530

    Done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
  
       <div className='h-[70vh] :h-[80vh] lg:bg-green-100 flex lg:-my-14 home2 '>
      <div className=' box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-r-[100px] flex flex-col justify-center '> 
-      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <section className='p-7 md:p-10 lg:p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
       <p className='boxHeading text-xl md:text-2xl lg:text-4xl font-sans font-semibold flex flex-wrap lg:tracking-wide mb-6'>Join our community in creating a more sustainable future for everyone</p>
      <p className='text-md lg:text-xl lg:mb-4 lg:flex-wrap '>At Ecoyaan, we are more than just a platform. Our vision is to build an eco-conscious community for people and products.</p>
      <p className='text-md lg:text-xl flex-wrap pb-4'>Consider us your partner for all things sustainable. To keep you motivated on the journey, we will provide you with engaging information about climate change, the latest updates on climate policies and lifestyle tips that you can adopt to reduce your impact on the environment.</p>
@@ -64,9 +64,9 @@ const Home = () => {
       
       
       <div className='h-[100vh] lg:h-[80vh] lg:bg-green-100 flex lg:-my-14 home2'>
-      <img src={image2} alt='image2' className=' joinImage h-[50vh] lg:w-[40%] relative lg:top-[18%] top-[70%] rounded-full p-10'/>  
+      <img src={image2} alt='image2' className=' joinImage h-[50vh] lg:w-[40%] relative lg:top-[18%] top-[70%] rounded-full p-10 '/>  
      <div className='box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-l-[100px] flex flex-col justify-center relative lg:-top-0 -top-64'> 
-      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <section className='p-7 md:p-10 lg:p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
       <p className='text-xl lg:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Carefully curated Eco-friendly products</p>
      <p className='text-md lg:text-xl mb-4 flex-wrap '>We believe that every purchase you make can have a positive impact on the planet and the future.</p>
      <p className='text-md lg:text-xl flex-wrap '>Soon, we will be connecting you with eco-friendly and sustainable products that are carefully selected based on their environmental and social benefits.</p>
@@ -76,7 +76,7 @@ const Home = () => {
 
       <div className='h-[120vh] lg:h-[80vh] lg:bg-green-100 flex lg:-my-14 home2 '>
      <div className='box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-r-[100px] flex flex-col justify-center '> 
-      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <section className='p-7 md:p-10 lg:p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
       <p className='text-xl lg:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>For businesses that care deeply about sustainability</p>
      <p className='text-md lg:text-xl mb-4 flex flex-wrap '>Do you run a business that is committed to sustainability in every aspect of your work?</p>
      <p className='text-md lg:text-xl mb-4 flex flex-wrap'>Do you want to showcase your products to an incredible audience of eco-conscious consumers who care?</p>
