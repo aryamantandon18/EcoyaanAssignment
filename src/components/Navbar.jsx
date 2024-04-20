792f03b14adbc18e4ab767534fe54015689a8416
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-04-20 19:53:53 +0530

    finalC

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,27 +26,27 @@ const Navbar = () => {
   }, [windowWidth]);
   return (
    <>
-    <div className='md:hidden h-[6vh] md:h-[80px] max-w-screen w-full flex items-center shadow-xl z-10 fixed t-0 bg-white'>
+    <div className='lg:hidden h-[6vh] lg:h-[80px] max-w-screen w-full flex items-center shadow-xl z-10 fixed t-0 bg-white'>
     <FaBars className='w-16 text-2xl cursor-pointer  hover:text-green-500 ' onClick={showNavbar}/>
-    <p className='text-2xl md:text-5xl font-sans font-semibold hover:text-green-500 cursor-pointer hover:scale-105 transition ease delay-80'>Ecoyaan</p>
+    <p className='text-2xl lg:text-5xl font-sans font-semibold hover:text-green-500 cursor-pointer hover:scale-105 transition ease delay-80'>Ecoyaan</p>
     </div>
-    <div className='navbar md:h-[80px] max-w-screen md:w-full flex md:justify-between shadow-xl z-10 fixed t-0 bg-white '
+    <div className='navbar lg:h-[80px] max-w-screen lg:w-full flex lg:justify-between shadow-xl z-10 fixed t-0 bg-white '
     ref={navRef}  >
-    <div className='items-center flex md:gap-5'>
-      {windowWidth<768 ? ( <div><FaTimes className='w-16 text-3xl  hover:text-green-500 cursor-pointer  transition ease delay-80 text-' onClick={showNavbar}/> </div>  ) :(
-          <img src='https://tse2.mm.bing.net/th?id=OIP.zHP9bm5ZKQ2syBgQxfPOjgHaGx&pid=Api&P=0&h=180' className='w-16 hidden md:block'/>
+    <div className='items-center flex lg:gap-5'>
+      {windowWidth<1000 ? ( <div><FaTimes className='w-16 text-3xl  hover:text-green-500 cursor-pointer  transition ease delay-80 text-' onClick={showNavbar}/> </div>  ) :(
+          <img src='https://tse2.mm.bing.net/th?id=OIP.zHP9bm5ZKQ2syBgQxfPOjgHaGx&pid=Api&P=0&h=180' className='w-16 hidden lg:block'/>
       )}
-        <p className='text-2xl md:text-5xl font-sans font-semibold hover:text-green-500 cursor-pointer hover:scale-105 transition ease delay-80'>Ecoyaan</p>
+        <p className='text-2xl lg:text-5xl font-sans font-semibold hover:text-green-500 cursor-pointer hover:scale-105 transition ease delay-80'>Ecoyaan</p>
     </div>
-    <div className='navbarLinks font-sans flex md:gap-8 items-center text-lg md:text-2xl'>
-        <Link className='transition ease-in-out delay-80  p-2 md:hover:underline underline-offset-[29px] md:font-semibold focus:font-bold  hover:font-bold' to={"/"}>Home</Link>
-        <Link className=' transition ease-in-out delay-80 p-2 md:hover:underline underline-offset-[29px] md:font-semibold focus:font-bold hover:font-bold' to={"/about"}>About</Link>
-        <Link className=' transition ease-in-out delay-80 p-2 md:hover:underline underline-offset-[29px] md:font-semibold focus:font-bold  hover:font-bold'>Contact</Link>
-        <Link className='transition ease-in-out delay-80 p-2 md:hover:underline underline-offset-[29px] md:font-semibold focus:font-bold  hover:font-bold'>Others</Link>
+    <div className='navbarLinks font-sans flex lg:gap-8 items-center text-lg lg:text-2xl'>
+        <Link className='transition ease-in-out delay-80  p-2 lg:hover:underline underline-offset-[29px] lg:font-semibold focus:font-bold  hover:font-bold' to={"/"}>Home</Link>
+        <Link className=' transition ease-in-out delay-80 p-2 lg:hover:underline underline-offset-[29px] lg:font-semibold focus:font-bold hover:font-bold' to={"/about"}>About</Link>
+        <Link className=' transition ease-in-out delay-80 p-2 lg:hover:underline underline-offset-[29px] lg:font-semibold focus:font-bold  hover:font-bold'>Contact</Link>
+        <Link className='transition ease-in-out delay-80 p-2 lg:hover:underline underline-offset-[29px] lg:font-semibold focus:font-bold  hover:font-bold'>Others</Link>
     </div>
-    <div className='flex items-center md:p-2 md:gap-6 flex-col md:flex-row navbtnContainer' ref={navRef}>
-        <button className='navbtn items-center class="md:w-20 md:px-4 md:py-3 md:bg-green-600 rounded-md md:text-white font-sans md:text-xl outline-none md:focus:ring-4 md:shadow-lg md:transform active:scale-y-75 md:transition-transform flex text-lg'>LogIn</button>
-        <button className='navbtn items-center class="md:w-20 md:px-4 md:py-3 md:bg-green-600 rounded-md md:text-white font-sans md:text-xl outline-none md:focus:ring-4 md:shadow-lg md:transform active:scale-y-75 md:transition-transform flex text-lg'>SignUp</button>
+    <div className='flex items-center lg:p-2 lg:gap-6 flex-col lg:flex-row navbtnContainer' ref={navRef}>
+        <button className='navbtn items-center class="lg:w-20 lg:px-4 lg:py-3 lg:bg-green-600 rounded-md lg:text-white font-sans lg:text-xl outline-none lg:focus:ring-4 lg:shadow-lg lg:transform active:scale-y-75 lg:transition-transform flex text-lg'>LogIn</button>
+        <button className='navbtn items-center class="lg:w-20 lg:px-4 lg:py-3 lg:bg-green-600 rounded-md lg:text-white font-sans lg:text-xl outline-none lg:focus:ring-4 lg:shadow-lg lg:transform active:scale-y-75 lg:transition-transform flex text-lg'>SignUp</button>
     </div>  
 
 </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       <div className=' w-[100%] h-[70%] bg-[url("https://ecoyaan.com/images/carousel-1.png")] bg-cover bg-center' >
          <div className='absolute left-[14%] top-[24%] banDiv flex flex-col items-start'>
          <p className='font-bold tracking-wide  text-4xl my-3 heading'>Buy Less,Buy Better!</p>
-          <p className='md:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
+          <p className='lg:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
           <Link to={"/about"}><button className='bg-green-600 w-32 h-12 text-white rounded-3xl text-lg font-medium hover:bg-green-700 cursor-pointer btn'>About Us</button></Link>
          </div>
         </div>
@@ -32,7 +32,7 @@ const Home = () => {
         <div className='w-[100%] h-[70%] bg-[url("https://ecoyaan.com/images/carousel-2.png")] bg-cover bg-center' >
         <div className='absolute left-[14%] top-[24%] banDiv flex flex-col items-start'>
          <p className='font-bold tracking-wide  text-4xl my-3 heading'>Buy Less,Buy Better!</p>
-          <p className='md:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
+          <p className='lg:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
          <button className='bg-green-600 w-32 h-12 text-white rounded-3xl text-lg font-medium hover:bg-green-700 cursor-pointer btn'>About Us</button>
          </div>
         </div>
@@ -40,7 +40,7 @@ const Home = () => {
         <div className='w-[100%] h-[70%] bg-[url("https://ecoyaan.com/images/carousel-3.png")] bg-cover bg-center' >
         <div className='absolute left-[14%] top-[24%] banDiv flex flex-col items-start'>
          <p className='font-bold tracking-wide  text-4xl my-3 heading'>Buy Less,Buy Better!</p>
-          <p className='md:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
+          <p className='lg:text-2xl my-4 para'>Authentic source of truth for your sustainability needs.</p>
         <Link to={"/about"}><button className='bg-green-600 w-32 h-12 text-white rounded-3xl text-lg font-medium hover:bg-green-700 cursor-pointer btn'>About Us</button></Link>
          </div>
         </div>
@@ -49,44 +49,44 @@ const Home = () => {
         </div>
       </Carousel> 
      </div>
-
-      <div className='h-[70vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2 '>
-     <div className=' box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-r-[100px] flex flex-col justify-center '> 
-      <section className='p-7 md:w-[70%] mx-auto md:pr-28 text-justify'>
-      <p className='boxHeading text-xl md:text-4xl font-sans font-semibold flex flex-wrap md:tracking-wide mb-6'>Join our community in creating a more sustainable future for everyone</p>
-     <p className='text-md md:text-xl md:mb-4 md:flex-wrap '>At Ecoyaan, we are more than just a platform. Our vision is to build an eco-conscious community for people and products.</p>
-     <p className='text-md md:text-xl flex-wrap pb-4'>Consider us your partner for all things sustainable. To keep you motivated on the journey, we will provide you with engaging information about climate change, the latest updates on climate policies and lifestyle tips that you can adopt to reduce your impact on the environment.</p>
+ 
+      <div className='h-[70vh] :h-[80vh] lg:bg-green-100 flex lg:-my-14 home2 '>
+     <div className=' box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-r-[100px] flex flex-col justify-center '> 
+      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <p className='boxHeading text-xl lg:text-4xl font-sans font-semibold flex flex-wrap lg:tracking-wide mb-6'>Join our community in creating a more sustainable future for everyone</p>
+     <p className='text-md lg:text-xl lg:mb-4 lg:flex-wrap '>At Ecoyaan, we are more than just a platform. Our vision is to build an eco-conscious community for people and products.</p>
+     <p className='text-md lg:text-xl flex-wrap pb-4'>Consider us your partner for all things sustainable. To keep you motivated on the journey, we will provide you with engaging information about climate change, the latest updates on climate policies and lifestyle tips that you can adopt to reduce your impact on the environment.</p>
       </section>
      </div>
 
-     <img src='https://ecoyaan.com/images/Impact.png' alt='image2' className=' joinImage md:h-[46vh] md:w-[40%] relative md:top-[18%] top-[13%]'/>     
+     <img src='https://ecoyaan.com/images/Impact.png' alt='image2' className=' joinImage lg:h-[46vh] lg:w-[40%] relative lg:top-[18%] top-[13%]'/>     
       </div>
       
       
-      <div className='h-[100vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2'>
-      <img src={image2} alt='image2' className=' joinImage h-[50vh] md:w-[40%] relative md:top-[18%] top-[70%] rounded-full p-10'/>  
-     <div className='box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-l-[100px] flex flex-col justify-center relative md:-top-0 -top-64'> 
-      <section className='p-7 md:w-[70%] mx-auto md:pr-28 text-justify'>
-      <p className='text-xl md:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Carefully curated Eco-friendly products</p>
-     <p className='text-md md:text-xl mb-4 flex-wrap '>We believe that every purchase you make can have a positive impact on the planet and the future.</p>
-     <p className='text-md md:text-xl flex-wrap '>Soon, we will be connecting you with eco-friendly and sustainable products that are carefully selected based on their environmental and social benefits.</p>
+      <div className='h-[100vh] lg:h-[80vh] lg:bg-green-100 flex lg:-my-14 home2'>
+      <img src={image2} alt='image2' className=' joinImage h-[50vh] lg:w-[40%] relative lg:top-[18%] top-[70%] rounded-full p-10'/>  
+     <div className='box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-l-[100px] flex flex-col justify-center relative lg:-top-0 -top-64'> 
+      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <p className='text-xl lg:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>Carefully curated Eco-friendly products</p>
+     <p className='text-md lg:text-xl mb-4 flex-wrap '>We believe that every purchase you make can have a positive impact on the planet and the future.</p>
+     <p className='text-md lg:text-xl flex-wrap '>Soon, we will be connecting you with eco-friendly and sustainable products that are carefully selected based on their environmental and social benefits.</p>
       </section>
      </div>   
       </div>
 
-      <div className='h-[120vh] md:h-[80vh] md:bg-green-100 flex md:-my-14 home2 '>
-     <div className='box w-full md:w-[60%] bg-white h-[70%] my-auto md:rounded-r-[100px] flex flex-col justify-center '> 
-      <section className='p-7 md:w-[70%] mx-auto md:pr-28 text-justify'>
-      <p className='text-xl md:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>For businesses that care deeply about sustainability</p>
-     <p className='text-md md:text-xl mb-4 flex flex-wrap '>Do you run a business that is committed to sustainability in every aspect of your work?</p>
-     <p className='text-md md:text-xl mb-4 flex flex-wrap'>Do you want to showcase your products to an incredible audience of eco-conscious consumers who care?</p>
-     <p className='text-md md:text-xl flex flex-wrap mb-5'>If yes, then you are the perfect fit for our platform. Our community appreciates and supports businesses that are transparent, ethical, and innovative in their approach to sustainability. Contact us today:</p>
-     <button className='bg-green-600 md:w-32 w-24 md:h-12 text-white rounded-3xl md:text-lg font-medium hover:bg-green-700 cursor-pointer p-2'>Contact us</button>
+      <div className='h-[120vh] lg:h-[80vh] lg:bg-green-100 flex lg:-my-14 home2 '>
+     <div className='box w-full lg:w-[60%] bg-white h-[70%] my-auto lg:rounded-r-[100px] flex flex-col justify-center '> 
+      <section className='p-7 lg:w-[70%] mx-auto lg:pr-28 text-justify'>
+      <p className='text-xl lg:text-4xl font-sans font-semibold flex flex-wrap tracking-wide mb-6 '>For businesses that care deeply about sustainability</p>
+     <p className='text-md lg:text-xl mb-4 flex flex-wrap '>Do you run a business that is committed to sustainability in every aspect of your work?</p>
+     <p className='text-md lg:text-xl mb-4 flex flex-wrap'>Do you want to showcase your products to an incredible audience of eco-conscious consumers who care?</p>
+     <p className='text-md lg:text-xl flex flex-wrap mb-5'>If yes, then you are the perfect fit for our platform. Our community appreciates and supports businesses that are transparent, ethical, and innovative in their approach to sustainability. Contact us today:</p>
+     <button className='bg-green-600 lg:w-32 w-24 lg:h-12 text-white rounded-3xl lg:text-lg font-medium hover:bg-green-700 cursor-pointer p-2'>Contact us</button>
       </section>
      </div>
 
-      <div className='md:w-[40%]'>
-      <img src='https://ecoyaan.com/images/OurStory4.png' alt='image2' className=' mx-auto joinImage md:h-[46vh] md:w-[80%] relative md:top-[18%] -top-16'/>     
+      <div className='lg:w-[40%]'>
+      <img src='https://ecoyaan.com/images/OurStory4.png' alt='image2' className=' mx-auto joinImage lg:h-[46vh] lg:w-[80%] relative lg:top-[18%] -top-16'/>     
       </div>
       </div>
     </>
